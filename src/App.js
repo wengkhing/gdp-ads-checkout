e3142c9e7b62e16cc133c992b261cee0cea7e1e4
wengkhing <wengkhing@dd.express>
2017-12-11 18:32:08 +0800

    build(style): include sass and postcss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="card">
-          <h1>title</h1>
+          <h1>Title <small>subtitle</small></h1>
+          <h2>Title <small>subtitle</small></h2>
+          <h3>Title <small>subtitle</small></h3>
+          <h4>Title <small>subtitle</small></h4>
+          <h5>Title <small>subtitle</small></h5>
+          <h6>Title <small>subtitle</small></h6>
+          <p>Paragraph</p>
         </div>
       </div>
     );
