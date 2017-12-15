31cb8b80c86e46f34f2381d06582c2d87e272ae6
wengkhing <wengkhing@gmail.com>
2017-12-15 08:20:18 +0800

    test(reducer): app reducer

---
+++ src/reducers/app.reducer.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import reducer from './app.reducer';
+
+import {
+  APP_LOGIN,
+  APP_LOGOUT,
+  PRODUCT_LIST
+} from '../actions/actionTypes'
+
+import products from '../data/products'
+
+const initialState = {
+  page: '',
+  products: {},
+  user: null
+}
+
+describe('app reducers', () => {
+
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('[APP_LOGIN] should able to login', () => {
+    expect(reducer(initialState, {
+      type: APP_LOGIN,
+      payload: { user_id: 'apple'}
+    }).user.id).toEqual('apple')
+  })
+
+  it('[APP_LOGOUT] should able to logout', () => {
+    expect(reducer(initialState, {
+      type: APP_LOGOUT
+    }).user).toEqual(null)
+  })
+
+  it('[PRODUCT_LIST] should able to logout', () => {
+    expect(reducer(initialState, {
+      type: PRODUCT_LIST
+    }).products).toEqual(products)
+  })
+})
